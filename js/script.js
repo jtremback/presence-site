32fa443fa85a89d729ede6d0857da3e02bec1300
Jehan <jehan.tremback@gmail.com>
2013-11-18 17:17:21 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $('.video-background').videobackground({
+//   videoSource: [['/video/stream.mp4', 'video/mp4']],
+//     // ['video/big-buck-bunny.webm', 'video/webm'],
+//     // ['video/big-buck-bunny.ogv', 'video/ogg']],
+//   // controlPosition: '#main',
+//   // poster: 'video/big-buck-bunny.jpg',
+//   resize: true,
+//   resizeTo: 'window',
+//   loop: true,
+//   loadedCallback: function() {
+//     $(this).videobackground('mute');
+//   }
+// });
