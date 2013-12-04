4e973f82af9e651a0f1cdea74f93b43cb05790cb
Jehan <jehan.tremback@gmail.com>
2013-12-04 14:55:05 -0800

    first version

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-// $('.video-background').videobackground({
-//   videoSource: [['/video/stream.mp4', 'video/mp4']],
+/*global $*/
+
+// $('.bg-video').videobackground({
+//   videoSource: [['./images/traffic.mp4', 'video/mp4']],
 //     // ['video/big-buck-bunny.webm', 'video/webm'],
 //     // ['video/big-buck-bunny.ogv', 'video/ogg']],
 //   // controlPosition: '#main',
 //   // poster: 'video/big-buck-bunny.jpg',
-//   resize: true,
+//   resize: false,
 //   resizeTo: 'window',
 //   loop: true,
 //   loadedCallback: function() {
 //     $(this).videobackground('mute');
 //   }
 // });
+
+// $(window).scroll(function(e){
+//   parallax();
+// });
+
+// function parallax(){
+//   var scrolled = $(window).scrollTop();
+//   $('.js-parallax').css('background-position', '50%' + ((scrolled*0.05))+'%');
+// }
