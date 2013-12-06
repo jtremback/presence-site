9d004003d1504af50b49de805f02d135c0ef3d46
Jehan <jehan.tremback@gmail.com>
2013-12-06 09:14:19 -0800

    new videos, many visual improvements

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 /*global $*/
 
 // $('.bg-video').videobackground({
@@ -14,11 +15,42 @@
 //   }
 // });
 
-// $(window).scroll(function(e){
-//   parallax();
-// });
+$(window).scroll(function(e){
+  parallax($('.js-parallax'));
+});
 
-// function parallax(){
-//   var scrolled = $(window).scrollTop();
-//   $('.js-parallax').css('background-position', '50%' + ((scrolled*0.05))+'%');
-// }
+function parallax($el){
+  // var scrolled = $(window).scrollTop();
+  $el.each(function() {
+    var element_offset = $(this).offset().top - $(window).scrollTop();
+    $(this).css('background-position', '50%' + (element_offset * 0.05) + '%');
+  });
+}
+
+var videos = [
+  'http://vimeo.com/81169230/download?t=1386311114&v=210281169&s=e1b2db56a10da3082363186b61282de0',
+  'http://vimeo.com/81169229/download?t=1386311053&v=210283943&s=ab0eae412a497d8b9847172a373da806',
+  'http://vimeo.com/81168830/download?t=1386311157&v=210278977&s=e2c505e1bc797edc3ec58060478e8f26',
+  'http://vimeo.com/81169228/download?t=1386312293&v=210288847&s=0a75cb2a3097fc49ea0552c19f5cdd31'
+];
+
+function insertVideo(videoEl, videos) {
+  var chosen_video = videos[Math.floor(Math.random()*videos.length)];
+  videoEl.innerHTML =
+    '<source src="' + chosen_video + '" type="video/mp4" />'
+    // + '<source src="/resources/video/product-hero.webmhd.webm" type="video/webm" />'
+    // + '<img src="/images/product/product-parent-hero.jpg" title="Your browser does not support the <video> tag">'
+  ;
+}
+
+insertVideo(document.getElementById('bg-video'), videos);
+
+
+// <video autoplay>
+//     <source src="/resources/video/product-hero.mp4.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+//     <source src="/resources/video/product-hero.webmhd.webm" type='video/webm; codecs="vp8, vorbis"' />
+//     <img src="/images/product/product-parent-hero.jpg" title="Your browser does not support the <video> tag">
+// </video>
+
+// video.bg-video(autoplay="autoplay", loop="loop")
+//   source(src="./images/RainingH264.mp4", type="video/mp4")
