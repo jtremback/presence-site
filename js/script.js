e90e6275d087fed4b7df0964cd487a8ad9c817e2
Jehan <jehan.tremback@gmail.com>
2013-12-10 11:16:18 -0800

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
 'use strict';
 /*global $*/
 
-// $('.bg-video').videobackground({
-//   videoSource: [['./images/traffic.mp4', 'video/mp4']],
-//     // ['video/big-buck-bunny.webm', 'video/webm'],
-//     // ['video/big-buck-bunny.ogv', 'video/ogg']],
-//   // controlPosition: '#main',
-//   // poster: 'video/big-buck-bunny.jpg',
-//   resize: false,
-//   resizeTo: 'window',
-//   loop: true,
-//   loadedCallback: function() {
-//     $(this).videobackground('mute');
-//   }
-// });
-
 $(window).scroll(function(e){
   parallax($('.js-parallax'));
 });
 
 function parallax($el){
-  // var scrolled = $(window).scrollTop();
   $el.each(function() {
     var element_offset = $(this).offset().top - $(window).scrollTop();
     $(this).css('background-position', '50%' + (element_offset * 0.05) + '%');
@@ -35,23 +20,12 @@ var videos = [
 
 function insertVideo(videoEl, videos) {
   var chosen = videos[Math.floor(Math.random()*videos.length)];
-  // alert(chosen.video);
   $(videoEl).attr('poster', chosen.still);
   videoEl.innerHTML =
-    '<source src="' + chosen.mp4 + '" type="video/mp4" />'
+      '<source src="' + chosen.mp4 + '" type="video/mp4" />'
     + '<source src="' + chosen.webm + '" type="video/webm" />'
     + '<img src="' + chosen.still + '">'
   ;
 }
 
 insertVideo(document.getElementById('bg-video'), videos);
-
-
-// <video autoplay>
-//     <source src="/resources/video/product-hero.mp4.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-//     <source src="/resources/video/product-hero.webmhd.webm" type='video/webm; codecs="vp8, vorbis"' />
-//     <img src="/images/product/product-parent-hero.jpg" title="Your browser does not support the <video> tag">
-// </video>
-
-// video.bg-video(autoplay="autoplay", loop="loop")
-//   source(src="./images/RainingH264.mp4", type="video/mp4")
